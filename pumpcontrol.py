b78e080baa809c7f5b061e395603ec5708fc9081
Arthaloith <nguyenmanhhung1904@gmail.com>
2024-03-20 14:39:10 +0700

    add moisture profile for each growth phase for durian tree

--- pumpcontrol.py
+++ pumpcontrol.py
@@ -1,3 +1,8 @@
+#========================================================================================================#
+#                                                                                                        #
+#                                               LIBRARY IMPORTS                                          #
+#                                                                                                        #
+#========================================================================================================#
 import sys
 import gpiozero
 import time
@@ -11,7 +16,12 @@ import board
 import math
 from adafruit_mcp3xxx.analog_in import AnalogIn
 from consts import RELAY_GPIO, PUMP_DURATION, PUMP_RUN_INTERVAL
-
+#========================================================================================================#
+#========================================================================================================#
+#                                                                                                        #
+#                                               HARDWARE SETUPS                                          #
+#                                                                                                        #
+#========================================================================================================#
 #setup relay
 pumpRelay = gpiozero.OutputDevice(RELAY_GPIO, active_high = False, initial_value = False)
 
@@ -24,7 +34,9 @@ cs = DigitalInOut(board.CE0)
 # Create MCP3008 object
 mcp = MCP.MCP3008(spi, cs)
 
+# Set up analog input channel
 chan = AnalogIn(mcp, MCP.P0)
+#========================================================================================================#
 
 def poll():
     lastLog = db.get_latest_pump_run()
@@ -61,40 +73,26 @@ def skipPump():
 
 def get_soil_moisture():
     soil_value = chan.value / 64
-    moisture_percentage = math.ceil((soil_value / 1024) * 100)
+    moisture_percentage = 100 - math.ceil((soil_value / 1024) * 100)
     return moisture_percentage
 
 def test():
     print( get_soil_moisture())
 
-# def activateChecknPump():
-#     soil_value = get_soil_moisture()
-#     if soil_value >= 50:
-#         print("soil too sus, watering...")
-#         print(soil_value)
-#         runPump()
-#     else:
-#         readable_timestamp = datetime.now().strftime('%Y-%m-%d %H:%M:%S')
-#         duration_str = f'{consts.PUMP_DURATION} seconds'
 
-#         entry = (readable_timestamp, 'Pump Run (failed, too wet)', duration_str)
-        
-#         db.log_pump_run(entry)
 def activateChecknPump():
     profile = db.get_active_profile()
     if not profile:
-        print("No profile found. Using default threshold of 50.")
         soil_moisture_threshold = 50
     else:
         soil_moisture_threshold = profile['soil_moisture_threshold']
     
     soil_value = get_soil_moisture()
-    if soil_value >= soil_moisture_threshold:
-        print("Soil too dry, watering...")
+    if soil_value > soil_moisture_threshold:
         runPump()
     else:
         readable_timestamp = datetime.now().strftime('%Y-%m-%d %H:%M:%S')
-        duration_str = f'{consts.PUMP_DURATION} seconds'
+        duration_str = 'N/A'
 
         entry = (readable_timestamp, 'Pump Run Failed due too moisture still too high', duration_str)
         db.log_pump_run(entry)
@@ -104,7 +102,75 @@ def heartBeat():
     entry = (readable_timestamp, 'Heartbeat', '1')
     
     db.log_pump_run(entry)
-    
+#========================================================================================================#
+#                                                                                                        #
+#                                               DURIAN PROFILES                                          #
+#                                                                                                        #
+#========================================================================================================#
+# Phase 1: small plant
+def phaseOne():
+    soil_value = get_soil_moisture()
+    if 65 <= soil_value <= 80:
+        pumpRelay.on()
+        time.sleep(14)
+        pumpRelay.off()
+        
+        readable_timestamp = datetime.now().strftime('%Y-%m-%d %H:%M:%S')
+        duration_str = '14 seconds'
+
+        entry = (readable_timestamp, 'Pump Run', duration_str)
+        
+        db.log_pump_run(entry)
+    else:
+        readable_timestamp = datetime.now().strftime('%Y-%m-%d %H:%M:%S')
+        duration_str = 'N/A'
+
+        entry = (readable_timestamp, 'Pump Run (failed, too wet)', duration_str)
+        
+        db.log_pump_run(entry)
+# Phase 2: blooming plant
+def phaseTwo():
+    soil_value = get_soil_moisture()
+    if 50 <= soil_value <= 60:
+        pumpRelay.on()
+        time.sleep(14)
+        pumpRelay.off()
+        
+        readable_timestamp = datetime.now().strftime('%Y-%m-%d %H:%M:%S')
+        duration_str = 'N/A'
+
+        entry = (readable_timestamp, 'Pump Run', duration_str)
+        
+        db.log_pump_run(entry)
+    else:
+        readable_timestamp = datetime.now().strftime('%Y-%m-%d %H:%M:%S')
+        duration_str = f'{consts.PUMP_DURATION} seconds'
+
+        entry = (readable_timestamp, 'Pump Run (failed, too wet)', duration_str)
+        
+        db.log_pump_run(entry)
+# Phase 3: fruiting plant
+def phaseThree():
+    soil_value = get_soil_moisture()
+    if soil_value < 80:
+        pumpRelay.on()
+        time.sleep(14)
+        pumpRelay.off()
+        
+        readable_timestamp = datetime.now().strftime('%Y-%m-%d %H:%M:%S')
+        duration_str = 'N/A'
+
+        entry = (readable_timestamp, 'Pump Run', duration_str)
+        
+        db.log_pump_run(entry)
+    else:
+        readable_timestamp = datetime.now().strftime('%Y-%m-%d %H:%M:%S')
+        duration_str = f'{consts.PUMP_DURATION} seconds'
+
+        entry = (readable_timestamp, 'Pump Run (failed, too wet)', duration_str)
+        
+        db.log_pump_run(entry)
+        
 if __name__ == "__main__":
     if len(sys.argv) == 2:
         if sys.argv[1] == 'runnow':
@@ -119,6 +185,12 @@ if __name__ == "__main__":
             skipPump()
         elif sys.argv[1] == 'dokkyun':
             heartBeat()
+        elif sys.argv[1] == 'p1':
+            phaseOne()
+        elif sys.argv[1] == 'p2':
+            phaseTwo()
+        elif sys.argv[1] == 'p3':
+            phaseThree()
         else:
             print("Invalid argument. Available options are 'runnow' or 'createdb'.")
     else:
