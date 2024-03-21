ec64863a565fe938363b55195990d4a4aa05a80b
Arthaloith <nguyenmanhhung1904@gmail.com>
2024-03-21 15:22:24 +0700

    added bash script to add cron jobs

---
+++ cronjob.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+# Add the reboot cron job
+(crontab -l ; echo "@reboot cd /home/admin/Projects/durianpi && /home/admin/Projects/durianpi/myenv/bin/python webserver.py &") | crontab -
+
+# Add the Durian profiles cron jobs
+(crontab -l ; echo "0 6,18 * 1-2 1-7 cd /home/admin/Projects/durianpi && /home/admin/Projects/durianpi/myenv/bin/python pumpcontrol -p1") | crontab -
+(crontab -l ; echo "0 6,18 * 3-4 1-7 cd /home/admin/Projects/durianpi && /home/admin/Projects/durianpi/myenv/bin/python pumpcontrol -p2") | crontab -
+(crontab -l ; echo "0 6,18 * 5-6 1-7 cd /home/admin/Projects/durianpi && /home/admin/Projects/durianpi/myenv/bin/python pumpcontrol -p3") | crontab -
+
+# Add the test profile cron job
+(crontab -l ; echo "0 10,18 * 1-2 1-7 cd /home/admin/Projects/durianpi && /home/admin/Projects/durianpi/myenv/bin/python pumpcontrol -p1") | crontab -
