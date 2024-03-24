000a1ff9501430d90d53dc2c19930c950c5828d0
Arthaloith <nguyenmanhhung1904@gmail.com>
2024-03-24 19:36:14 +0700

    rekted

--- speechreg.py
+++ speechreg.py
@@ -1,29 +1,26 @@
 import os
 import sys
 import speech_recognition as sr
-import subprocess  # Import subprocess module
+import subprocess 
 
-# Create a recognizer instance
 r = sr.Recognizer()
 
-# Specify the device index of the microphone you wish to use
 mic = sr.Microphone(device_index=1)
 
 print("Start talking...")
 
 while True:
     with mic as source:
-        # Optionally adjust for ambient noise
         r.adjust_for_ambient_noise(source)
         print("Listening...")
         audio = r.listen(source)
     try:
-        # Recognize speech using Google's speech recognition service
-        words = r.recognize_google(audio, language="vi-VN")
+
+        words = r.recognize_google(audio)
         print(words)
-        if words.lower() == "kích hoạt":  # Check if the recognized word is "hello"
+        if words.lower() == "activate":
             print("executing pumpcontrol.py...")
-            subprocess.run(["python", "durianpi/pumpcontrol.py", "runnow"])  # Execute pumpcontrol.py
+            subprocess.run(["python", "durianpi/pumpcontrol.py", "runnow"])
     except sr.UnknownValueError:
         print("Google Speech Recognition could not understand audio")
     except sr.RequestError as e:
