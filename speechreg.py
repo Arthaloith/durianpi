ee2b2f4e9db4c26127fccf2cea6aedacdaeb151b
Arthaloith <nguyenmanhhung1904@gmail.com>
2024-04-14 17:44:56 +0700

    idk

--- speechreg.py
+++ speechreg.py
@@ -8,19 +8,17 @@ r = sr.Recognizer()
 
 mic = sr.Microphone(device_index=1)
 
-print("Start talking...")
-
 while True:
     with mic as source:
-        r.adjust_for_ambient_noise(source)
+        r.adjust_for_ambient_noise(source, duration=1)
         print("Listening...")
         audio = r.listen(source)
     try:
-        words = r.recognize_google(audio, language='vi-VN')
+        words = r.recognize_google(audio)
         print(words)
-        if words.lower() == "tưới cây":
+        if words.lower() == "activate":
             print("executing pumpcontrol.py...")
-            subprocess.run(["python", "durianpi/pumpcontrol.py", "runnow"])
+            subprocess.run(["python", "/pumpcontrol.py", "runnow"])
     except sr.UnknownValueError:
         print("Google Speech Recognition could not understand audio")
     except sr.RequestError as e:
