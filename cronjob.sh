0801f304ec597b6a297955cb37b816fc67aa790e
Arthaloith <nguyenmanhhung1904@gmail.com>
2024-04-14 18:22:20 +0700

    wierd...

--- cronjob.sh
+++ cronjob.sh
@@ -2,7 +2,7 @@
 
 (crontab -l ; echo "@reboot cd /home/admin/Projects/durianpi && /home/admin/Projects/durianpi/myenv/bin/python webserver.py &") | crontab -
 
-(crontab -l ; echo "0 6,18 * 1-2 0-6 cd /home/admin/Projects/durianpi && /home/admin/Projects/durianpi/myenv/bin/python pumpcontrol p1") | crontab -
-(crontab -l ; echo "0 6,18 * 3-4 0-6 cd /home/admin/Projects/durianpi && /home/admin/Projects/durianpi/myenv/bin/python pumpcontrol p2") | crontab -
-(crontab -l ; echo "0 6,18 * 5-6 0-6 cd /home/admin/Projects/durianpi && /home/admin/Projects/durianpi/myenv/bin/python pumpcontrol p3") | crontab -
+(crontab -l ; echo "0 6,18 * 1-2 0-6 cd /home/admin/Projects/durianpi && /home/admin/Projects/durianpi/myenv/bin/python pumpcontrol.py p1") | crontab -
+(crontab -l ; echo "0 6,18 * 3-4 0-6 cd /home/admin/Projects/durianpi && /home/admin/Projects/durianpi/myenv/bin/python pumpcontrol.py p2") | crontab -
+(crontab -l ; echo "0 6,18 * 5-6 0-6 cd /home/admin/Projects/durianpi && /home/admin/Projects/durianpi/myenv/bin/python pumpcontrol.py p3") | crontab -
 
